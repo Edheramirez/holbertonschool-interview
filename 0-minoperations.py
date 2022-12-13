10784566d07ab9cb2f03d3a2308aa3c8ea2cd468
Edheramirez <https://github.com/Edheramirez>
2022-12-12 20:09:26 -0500

    Minimun Operations

---
+++ 0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+
+    if n <= 1:
+        return 0
+    num, div, numOfOperations = n, 2, 0
+
+    while num > 1:
+        if num % div == 0:
+            num = num / div
+            numOfOperations = numOfOperations + div
+        else:
+            div += 1
+    return numOfOperations
